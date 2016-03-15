f7326c7e63fe161f629c2155bf9ed837949a84f3
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-15 12:35:18 -0400

    Added landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  layout "static_application"
+
   def index
     @mainTitle = "Welcome in Petrohub Project."
     @mainDesc = "Fuel - Supply - Marketplace"
