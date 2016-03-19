e4d2e532d4afc656d489e4b1e09adf1043636826
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-19 00:56:07 -0400

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,101 @@ User.create(
   city: 'New York',
   state: 'New York',
   zip_code: '11367',
-  tax_id: '223103456',
-  ssn: '23456789',
+  tax_id: '2231034561',
+  ssn: '234567892',
   in_biz: '3',
-  email: 'apple@yahoo.com',
+  email: 'new@yahoo.com',
   password: 'password',
   password_confirmation: 'password',
-  terms: 1
-  )
+  terms: 1,
+  role: 0,
+  status: 0
+)
+
+User.create(
+  first_name: 'John',
+  last_name: 'Doe',
+  business_name: 'ACME inc',
+  phone_number: '2128456789',
+  cell_number: '2123456789',
+  street_address: '81 updown st',
+  apt_suite: '35',
+  city: 'New York',
+  state: 'New York',
+  zip_code: '11367',
+  tax_id: '2231034563',
+  ssn: '234567894',
+  in_biz: '3',
+  email: 'retailer@yahoo.com',
+  password: 'password',
+  password_confirmation: 'password',
+  terms: 1,
+  role: 1,
+  status: 1
+)
+
+User.create(
+  first_name: 'John',
+  last_name: 'Doe',
+  business_name: 'ACME inc',
+  phone_number: '2128456789',
+  cell_number: '2123456789',
+  street_address: '81 updown st',
+  apt_suite: '35',
+  city: 'New York',
+  state: 'New York',
+  zip_code: '11367',
+  tax_id: '2231034565',
+  ssn: '234567896',
+  in_biz: '3',
+  email: 'supplier@yahoo.com',
+  password: 'password',
+  password_confirmation: 'password',
+  terms: 1,
+  role: 2,
+  status: 1
+)
+
+User.create(
+  first_name: 'John',
+  last_name: 'Doe',
+  business_name: 'ACME inc',
+  phone_number: '2128456789',
+  cell_number: '2123456789',
+  street_address: '81 updown st',
+  apt_suite: '35',
+  city: 'New York',
+  state: 'New York',
+  zip_code: '11367',
+  tax_id: '2231034567',
+  ssn: '234567898',
+  in_biz: '3',
+  email: 'trucking@yahoo.com',
+  password: 'password',
+  password_confirmation: 'password',
+  terms: 1,
+  role: 3,
+  status: 1
+)
+
+User.create(
+  first_name: 'John',
+  last_name: 'Doe',
+  business_name: 'ACME inc',
+  phone_number: '2128456789',
+  cell_number: '2123456789',
+  street_address: '81 updown st',
+  apt_suite: '35',
+  city: 'New York',
+  state: 'New York',
+  zip_code: '11367',
+  tax_id: '2231034569',
+  ssn: '234567890',
+  in_biz: '3',
+  email: 'trucking@yahoo.com',
+  password: 'password',
+  password_confirmation: 'password',
+  terms: 1,
+  role: 4,
+  status: 1
+)
