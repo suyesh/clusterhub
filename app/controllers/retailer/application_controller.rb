cfdaeeec99901acd45005792409c9ef5bd48af98
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-19 01:01:01 -0400

    Added authorization in retailer

--- app/controllers/retailer/application_controller.rb
+++ app/controllers/retailer/application_controller.rb
@@ -1,4 +1,24 @@
 class Retailer::ApplicationController < LoginrequiredController
+  before_action :authenticate_retailer!
+
   def index
+  end
+
+  private
+
+  def dashboard_path
+    if current_user.admin?
+      admin_dashboard_path
+    elsif current_user.supplier?
+      supplier_dashboard_path
+    elsif current_user.trucking?
+      trucking_dashboard_path
+    end
+  end
+
+  def authenticate_retailer!
+    if !current_user.retailer?
+      redirect_to dashboard_path, alert: "You are not authorized to access that section."
+    end
   end
 end
