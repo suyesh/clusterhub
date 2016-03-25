3af08fab8f4563ef612f5f81715908a08b899e59
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-24 21:58:30 -0400

    Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < ApplicationController
   layout "static_application"
 
   def index
-    @mainTitle = "Welcome in Petrohub Project."
-    @mainDesc = "Fuel - Supply - Marketplace"
   end
 
-  def minor
-  end
 end
