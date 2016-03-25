5c02a711cc32200b5184ff311e90338a1f28a24f
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-25 11:12:35 -0400

    Added contacts controller

--- app/controllers/supplier/retailers_controller.rb
+++ app/controllers/supplier/retailers_controller.rb
@@ -1,4 +1,36 @@
 class Supplier::RetailersController < Supplier::ApplicationController
   def index
+    @retailer = current_user.contacts.all
+  end
+
+  def new
+    @retailer = current_user.contacts.build
+    @latest_retailer = current_user.contacts.last
+  end
+
+  def create
+    @retailer = current_user.contacts.build(contacts_params)
+    if @retailer.save
+      flash[:notice] = 'Retailer has been successfully added.'
+      redirect_to supplier_retailers_path
+    else
+      flash.now[:alert] = 'Something went wrong. Check your form and re-try.'
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def contacts_params
+    params.require(:contact).permit(:first_name, :last_name, :business_name, :phone_number, :cell_number, :street_address, :apt_suite, :city, :state, :zip_code,:in_biz,:email)
   end
 end
