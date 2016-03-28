e456479964e36613295e21c4c7162b8f3cc1337c
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-28 10:05:40 -0400

    GMAIL env variables set

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,13 @@ Myapp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => ENV["GMAIL_USERNAME"]
+      :password             => ENV["GMAIL_PASSWORD"]
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+}
 end
