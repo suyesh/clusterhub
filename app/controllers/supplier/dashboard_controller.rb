21df46fe041cfe052a54b13429d510ac2715c018
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-06 21:05:47 -0400

    .

--- app/controllers/supplier/dashboard_controller.rb
+++ app/controllers/supplier/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Supplier::DashboardController < Supplier::ApplicationController
 
   def index
     @latest_fuel_price = current_user.fuel_prices.last
+    @texts = 750.00 #current_user.texts.all()
   end
 
 
