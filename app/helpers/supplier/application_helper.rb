a34b8659df1bc4d798b865ead9ae31a6e4ac7b5a
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-08 16:41:47 -0400

    COntact tweak

--- app/helpers/supplier/application_helper.rb
+++ app/helpers/supplier/application_helper.rb
@@ -1,2 +1,7 @@
 module Supplier::ApplicationHelper
+  def avatar_url(user)
+    default_url = "#{root_url}images/person_placeholder.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=200"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408173104) do
+ActiveRecord::Schema.define(version: 20160408200411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160408173104) do
     t.decimal  "c_delivery"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "email"
   end
 
   create_table "fuel_prices", force: :cascade do |t|
