b20e4ef739ec23923152b3e9af2670414fe3f739
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-08 14:28:56 -0400

    Average

--- app/helpers/supplier/dashboard_helper.rb
+++ app/helpers/supplier/dashboard_helper.rb
@@ -10,26 +10,29 @@ module Supplier::DashboardHelper
   end
 
   def calculate_average(current_user)
-    regulars = []
-    medium = []
-    premium = []
-    diesel = []
-    current_user.contacts.each do |contact|
-      regulars << contact.c_regular
-      medium << contact.c_medium
-      premium << contact.c_premium
-      diesel << contact.c_diesel
+    unless current_user.fuel_prices.empty? && current_user.contacts.empty?
+      regulars = []
+      medium = []
+      premium = []
+      diesel = []
+      current_user.contacts.each do |contact|
+        regulars << contact.c_regular
+        medium << contact.c_medium
+        premium << contact.c_premium
+        diesel << contact.c_diesel
+      end
+      av_regular =   regulars.inject(:+) / regulars.length
+      av_medium = medium.inject(:+) / medium.length
+      av_premium = premium.inject(:+) / premium.length
+      av_diesel = diesel.inject(:+)/ diesel.length
+      all_av = {
+        regular: av_regular,
+        medium: av_medium,
+        premium: av_premium,
+        diesel: av_diesel
+      }
+      return all_av[:regular]
     end
-    av_regular =   regulars.inject(:+) / regulars.length
-    av_medium = medium.inject(:+) / medium.length
-    av_premium = premium.inject(:+) / premium.length
-    av_diesel = diesel.inject(:+)/ diesel.length
-    all_av = {
-      regular: av_regular,
-      medium: av_medium,
-      premium: av_premium,
-      diesel: av_diesel
-    }
-    return all_av[:regular]
+    return 0
   end
 end
