3270bf3ca3ea2bab742990a7a57f2a24b91ebfc6
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-08 22:51:18 -0400

    .

--- app/helpers/supplier/application_helper.rb
+++ app/helpers/supplier/application_helper.rb
@@ -4,4 +4,9 @@ module Supplier::ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=200"
   end
+
+  def contact_full_name(contact)
+    full_name = contact.first_name + " " + contact.last_name
+    return full_name
+  end
 end
