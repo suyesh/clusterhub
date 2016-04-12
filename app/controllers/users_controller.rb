7c2659a1c4a1a8c6714b85318205f6c211f616ee
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-12 16:23:07 -0400

    >

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,12 +30,16 @@ class UsersController < ApplicationController
     @default_password = SecureRandom.hex(5)
   end
 
+  #def user_params
+  #  default_params = {
+  #  password: @default_password,
+  #  password_confirmation: @default_password
+  #}
+  #  params.require(:user).permit(:first_name, :last_name, :business_name, :phone_number, :cell_number, :street_address, :apt_suite, :city, :state, :zip_code, :tax_id, :ssn, :in_biz, :terms, :email, :password, :password_confirmation).merge(default_params)
+  #end
+
   def user_params
-    default_params = {
-    password: @default_password,
-    password_confirmation: @default_password
-  }
-    params.require(:user).permit(:first_name, :last_name, :business_name, :phone_number, :cell_number, :street_address, :apt_suite, :city, :state, :zip_code, :tax_id, :ssn, :in_biz, :terms, :email, :password, :password_confirmation).merge(default_params)
+    params.require(:user).permit(:first_name, :last_name, :business_name, :phone_number, :cell_number, :street_address, :apt_suite, :city, :state, :zip_code, :tax_id, :ssn, :in_biz, :terms, :email, :password, :password_confirmation)
   end
 
   def set_slack
