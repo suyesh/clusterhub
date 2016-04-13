8e95732c2dc7d8010f416312de2e5cda768c323e
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-13 12:26:35 -0400

    Added coffee script for fields

--- app/helpers/supplier/application_helper.rb
+++ app/helpers/supplier/application_helper.rb
@@ -8,4 +8,34 @@ module Supplier::ApplicationHelper
 #  def contact_full_name(contact)
   #  full_name = contact.first_name + " " + contact.last_name
   ##end
+  def fuel_types
+    [
+      '87 RFG',
+      '89 RFG',
+      '93 RFG',
+      'Low Sodium Diesel',
+      'Dyed ULS Diesel',
+      'Undyed ULS Diesel',
+      'Heating Oil',
+      'Kerosene'
+    ]
+  end
+
+  def link_to_add_fields(name, f, association)
+    # creates a new instance of the 'has_many' object
+    new_object = f.object.send(association).klass.new
+    # new_object = f.object.association.klass.new
+    #    f.object.association.klass # => Document
+    # new_object = f.object.documents.build Document(user_id: f.object.id)
+
+
+    # gets the object id
+    id = new_object.object_id
+
+    # creates a new form for the association
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end
