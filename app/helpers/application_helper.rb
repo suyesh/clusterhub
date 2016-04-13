d3a9c1a761bb6a4180a9b9be80ec9f309b2114f0
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-13 17:57:48 -0400

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,22 +19,4 @@ module ApplicationHelper
         end
 
     end
-
-    def link_to_add_fields(name, f, association)
-      # creates a new instance of the 'has_many' object
-      new_object = f.object.send(association).klass.new
-      # new_object = f.object.association.klass.new
-      #    f.object.association.klass # => Document
-      # new_object = f.object.documents.build Document(user_id: f.object.id)
-
-
-      # gets the object id
-      id = new_object.object_id
-
-      # creates a new form for the association
-      fields = f.fields_for(association, new_object, child_index: id) do |builder|
-        render(association.to_s + "_fields", f: builder)
-      end
-      link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
-    end
 end
