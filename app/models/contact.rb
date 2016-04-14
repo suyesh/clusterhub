41cf2a5e6142c3a4fa074bdfbca5dfdf0f7086a7
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-14 11:05:27 -0400

    Model removal and cleanup

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,37 +1,9 @@
-# create_table "contacts", force: :cascade do |t|
-#  t.string   "first_name"
-#  t.string   "last_name"
-#  t.string   "business_name"
-#  t.string   "phone_number"
-#  t.string   "cell_number"
-#  t.string   "street_address"
-#  t.string   "apt_suite"
-#  t.string   "city"
-#  t.string   "state"
-#  t.string   "zip_code"
-#  t.string   "in_biz"
-#  t.integer  "supplier_id"
-#  t.decimal  "c_regular"
-#  t.decimal  "c_medium"
-#  t.decimal  "c_premium"
-#  t.decimal  "c_diesel"
-#  t.decimal  "c_delivery"
-#  t.datetime "created_at",     null: false
-#  t.datetime "updated_at",     null: false
-# end
-
 class Contact < ActiveRecord::Base
   validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: 'Oops! Looks like the email you provided is not valid. Please fix and resubmit the form.'
   validates_presence_of :first_name
   validates_presence_of :email
   validates_presence_of :last_name
   validates_presence_of :cell_number
-  validates_presence_of :c_regular
-  validates_presence_of :c_medium
-  validates_presence_of :c_premium
-  validates_presence_of :c_diesel
-  validates_presence_of :c_delivery
-  after_save :send_message
   belongs_to :supplier, class_name: 'User'
   has_many :retail_prices
   has_many :price_rockets
--- app/models/fuel_price.rb
+++ app/models/fuel_price.rb
@@ -1,23 +1,5 @@
 class FuelPrice < ActiveRecord::Base
-  #after_save :create_contact_price
   belongs_to :supplier, class_name: 'User'
-  #belongs_to :retail_price
   has_many :fuel_products
   accepts_nested_attributes_for :fuel_products, allow_destroy: true
-
-
-=begin
-  def create_contact_price
-    unless supplier.contacts.empty?
-      supplier.contacts.each do |contact|
-        contact.retail_prices.create(r_regular: supplier.fuel_prices.last.regular + contact.c_regular,
-                                     r_medium: supplier.fuel_prices.last.medium + contact.c_medium,
-                                     r_premium: supplier.fuel_prices.last.premium + contact.c_premium,
-                                     r_diesel: supplier.fuel_prices.last.diesel + contact.c_diesel
-                                    )
-        supplier.pricerockets.create(status: :not_sent, to: contact.cell_number, body: "Hey there! #{contact.first_name}. #{supplier.first_name} from #{supplier.business_name} just updated the Gas price for today. Regular: $#{contact.retail_prices.last.r_regular}, Medium: $#{contact.retail_prices.last.r_medium},Premium: $#{contact.retail_prices.last.r_premium}, Diesel: $#{contact.retail_prices.last.r_diesel}")
-      end
-   end
-  end
-=end
 end
