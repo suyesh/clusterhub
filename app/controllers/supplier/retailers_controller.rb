936e87577fbb26985d90137deeeaeb45e2f9c2ce
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-20 10:55:42 -0400

    Contact controller

--- app/controllers/supplier/retailers_controller.rb
+++ app/controllers/supplier/retailers_controller.rb
@@ -1,18 +1,31 @@
 class Supplier::RetailersController < Supplier::ApplicationController
     def index
-        @retailers = User.where(role: :retailer)
+        @retailers = current_user.retailers.all
     end
 
     def new
-        @retailer = User.new
+        @retailer = current_user.retailers.build
+        @latest_retailer = current_user.retailers.last
     end
 
     def create
+        @retailer = current_user.retailers.new(retailers_params)
+        respond_to do |format|
+            format.js do
+                if @retailer.save
+                    @retailers = current_user.retailers.all.order('created_at DESC')
+                    flash.now[:notice] = 'Retailer has been successfully added.'
+                    render 'success'
+                else
+                    render 'create'
+                end
+            end
+        end
     end
 
     private
 
-    def retailer_params
+    def retailers_params
         default_params = {
             password: @default_password,
             password_confirmation: @default_password
