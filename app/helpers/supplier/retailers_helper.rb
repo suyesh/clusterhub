7f9b550f04a528b129f5d329e462d070f7cac0cd
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-23 10:34:07 -0400

    Retailer helper method

--- app/helpers/supplier/retailers_helper.rb
+++ app/helpers/supplier/retailers_helper.rb
@@ -24,4 +24,21 @@ module Supplier::RetailersHelper
           'PagePlus'
       ]
   end
+
+  def link_to_add_fields(name, f, association)
+      # creates a new instance of the 'has_many' object
+      new_object = f.object.send(association).klass.new
+      # new_object = f.object.association.klass.new
+      #    f.object.association.klass # => Document
+      # new_object = f.object.documents.build Document(user_id: f.object.id)
+
+      # gets the object id
+      id = new_object.object_id
+
+      # creates a new form for the association
+      fields = f.fields_for(association, new_object, child_index: id) do |builder|
+          render(association.to_s + '_fields', f: builder)
+      end
+      link_to(name, '#', class: 'btn btn-success  add_fields', data: { id: id, fields: fields.delete("\n") })
+  end
 end
